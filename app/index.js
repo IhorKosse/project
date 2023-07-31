7763afde10f5f9436701739ec0a9f484ee7f2e0c
Ihor Kosse <2856787@gmail.com>
2023-07-31 15:34:13 +0200

    first commit

---
+++ app/index.js
@@ -0,0 +1,20 @@
+// app/index.js
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({ host: 'redis' });
+
+app.get('/', (_req, res) => {
+  client.incr('visits', (err, visits) => {
+    if (err) {
+      console.error('Error incrementing visits:', err);
+      return res.status(500).send('Error incrementing visits');
+    }
+    res.send(`Hello! This page has been visited ${visits} times.`);
+  });
+});
+
+app.listen(80, () => {
+  console.log('App is running on port 80');
+});
